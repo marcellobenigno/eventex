2c6edb6a6e641ab141f98207e80a0dfa36e3f837
marcellobenigno <benigno.marcello@gmail.com>
2016-02-17 19:57:04 -0300

    Add Contacts

---
+++ eventex/core/tests/test_model_contact.py
@@ -0,0 +1,36 @@
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+from eventex.core.models import Speaker, Contact
+
+
+class ContactModelTest(TestCase):
+    def setUp(self):
+        self.speaker = Speaker.objects.create(
+            name='Henrique Bastos',
+            slug='henrique-bastos',
+            website='http://hbn.link/hb-pic'
+        )
+
+    def test_email(self):
+        contact = Contact.objects.create(
+            speaker=self.speaker,
+            kind=Contact.EMAIL,
+            value='henrique@bastos.net'
+        )
+        self.assertTrue(Contact.objects.exists())
+
+    def test_phone(self):
+        contact = Contact.objects.create(
+            speaker=self.speaker,
+            kind=Contact.PHONE,
+            value='83-987210486'
+        )
+        self.assertTrue(Contact.objects.exists())
+
+    def test_choices(self):
+        contact = Contact(speaker=self.speaker, kind='A', value='B')
+        self.assertRaises(ValidationError, contact.full_clean)
+
+    def test_str(self):
+        contact = Contact(speaker=self.speaker, kind=Contact.EMAIL, value='henrique@bastos.net')
+        self.assertEqual('henrique@bastos.net', str(contact))
